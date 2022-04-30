6cf4a403adce058ac0af8ae2dd0ec2565cc05ff7
migsilva89 <migdrum@gmail.com>
2022-04-30 22:46:11 +0200

    props vuejs

---
+++ src/components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="pt-4">
+        <div class="rounded-xl bg-orange-400">
+
+            <div class="pr-4">
+                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top bg-[url('/src/assets/images/icon-work.svg')] pt-6"></div>
+            </div>
+
+            <div class="rounded-xl bg-indigo-900 py-4">
+                <div class="flex justify-evenly items-center gap-12">
+                    <div class="text-white">
+                        <p class="text-xs">{{ title }}</p>
+                        <p class="text-2xl">{{ hours }}</p>
+                    </div>
+                    <div class="text-indigo-300">
+                        <div class="flex justify-end pb-4">
+                            <img class="w-4" src="@/assets/images/icon-ellipsis.svg" alt="">
+                        </div>
+                        <p class="text-xs">Last Week - 7hrs</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script setup> 
+
+defineProps({
+    
+    backgroundColor: {
+        type: String,
+        required: false
+    }, 
+
+    title: {
+        type: String,
+        required: false 
+    }, 
+    
+    hours: {
+        type: String,
+        required: false
+    },
+    
+    hoursLastWeek: {
+        type: String,
+        required: false
+    }
+
+})
+
+</script>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,58 +1,66 @@
 <template>
-<div class="flex justify-center bg-gray-900 h-screen">
-  <main class="pt-16">
+    <div class="flex justify-center bg-gray-900 h-screen">
+        <main class="pt-16">
 
-    <div class="rounded-xl bg-indigo-900">
-      <div class="rounded-xl bg-indigo-500 p-6 flex justify-center items-center text-white">
-        <div class="w-14 ">
-            <img class="border-2 rounded-full" src="@/assets/images/image-jeremy.png" alt="">
-        </div>
-        <div class="pl-6">
-            <p class="text-indigo-200 text-xs">Report for</p>
-            <h2 class="text-xl">Jeremy Robson</h2>
-        </div>
-      </div>
-
-      <div class="py-4 flex justify-center gap-8 text-sm text-indigo-300 ">
-        <button class=" hover:text-white">Daily</button>
-        <button class=" hover:text-white">Weekly</button>
-        <button class=" hover:text-white">Monthly</button>
-      </div>
-    </div>
-
-    <div class="pt-8">
-        <div class="rounded-xl bg-orange-400">
-            <div class="pr-4">
-                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top bg-[url('/src/assets/images/icon-work.svg')] pt-6"></div>
-            </div>
-            <div class="rounded-xl bg-indigo-900 py-4">
-                <div class="flex justify-evenly items-center gap-12">
-                    <div class="text-white">
-                        <p class="text-xs">Work</p>
-                        <p class="text-2xl">32hrs</p>
-                    </div>
-                    <div class="text-indigo-300">
-                        <div class="flex justify-end pb-4">
-                            <img class="w-4" src="@/assets/images/icon-ellipsis.svg" alt="">
-                        </div>
-                        <p class="text-xs">Last Week - 7hrs</p>
-                    </div>
+            <div class="rounded-xl bg-indigo-900">
+            <div class="rounded-xl bg-indigo-500 p-6 flex justify-center items-center text-white">
+                <div class="w-14 ">
+                    <img class="border-2 rounded-full" src="@/assets/images/image-jeremy.png" alt="">
+                </div>
+                <div class="pl-6">
+                    <p class="text-indigo-200 text-xs">Report for</p>
+                    <h2 class="text-xl">Jeremy Robson</h2>
                 </div>
             </div>
-        </div>
+
+            <div class="py-4 flex justify-center gap-8 text-sm text-indigo-300 ">
+                <button class=" hover:text-white">Daily</button>
+                <button class=" hover:text-white">Weekly</button>
+                <button class=" hover:text-white">Monthly</button>
+            </div>
+            </div>
+
+        <Card title="Work" hours="32hrs"/>
+
+
+
+        </main>
     </div>
-
-
-
-  </main>
-</div>
  
 </template>
 
 <script setup>
 import data from '@/data/data.json'
+import forEach from 'lodash/forEach'
+import Card from './Card.vue'
 
-console.log(data)
+const weekly = []
+const daily = []
+const monthly = []
+
+forEach(data, (item) => {
+    // console.log(item)
+    weekly.push({
+        title: item.title,
+        timeframes: item.timeframes.weekly,
+    })
+
+    daily.push({
+        title: item.title,
+        timeframes: item.timeframes.daily,
+    })
+
+     monthly.push({
+        title: item.title,
+        timeframes: item.timeframes.monthly,
+    })
+    
+})
+
+// console.log(data)
+console.log(weekly, daily, monthly)
+
+
 
 
 </script>
