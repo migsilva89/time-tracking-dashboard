008146816dcb2e064d5ce0ebe85eb0897b343a39
migsilva89 <migdrum@gmail.com>
2022-05-04 20:38:57 +0200

    functionality done!

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,35 +1,39 @@
 <template>
     <div class="pt-4">
-        <div class="rounded-xl bg-orange-400">
-
+        <div class="rounded-xl" :class="[backgroundColor]">
+ 
             <div class="pr-4">
-                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top bg-[url('/src/assets/images/icon-work.svg')] pt-6"></div>
+                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top pt-6" :class="[backgroundImage]"></div>
             </div>
 
             <div class="rounded-xl bg-indigo-900 py-4">
                 <div class="flex justify-evenly items-center gap-12">
                     <div class="text-white">
                         <p class="text-xs">{{ title }}</p>
-                        <p class="text-2xl">{{ hours }}</p>
+                        <p class="text-2xl">{{ hours }} hrs</p>
                     </div>
                     <div class="text-indigo-300">
                         <div class="flex justify-end pb-4">
                             <img class="w-4" src="@/assets/images/icon-ellipsis.svg" alt="">
                         </div>
-                        <p class="text-xs">Last Week - 7hrs</p>
+                        <p class="text-xs">Last Week - {{ hoursLastWeek }} hrs</p>
                     </div>
                 </div>
             </div>
 
         </div>
     </div>
-
 </template>
 
 <script setup> 
 
 defineProps({
     
+    backgroundImage: {
+        type: String,
+        required: false
+    },
+
     backgroundColor: {
         type: String,
         required: false
