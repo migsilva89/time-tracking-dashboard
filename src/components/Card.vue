bbb9f1d3c7fd820925f42dad3ed388b62580e3a0
migsilva89 <migdrum@gmail.com>
2022-05-08 17:48:49 +0200

    desktop style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="pt-4">
-        <div class="rounded-xl" :class="[backgroundColor]">
+    <div class="pt-4 lg:pt-0 mt-4 mb-4">
+        <div class="rounded-xl " :class="[backgroundColor]">
  
             <div class="pr-4">
-                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top pt-6" :class="[backgroundImage]"></div>
+                <div class="bg-[length:60px_60px] bg-no-repeat bg-right-top pt-6 lg:pt-10" :class="[backgroundImage]"></div>
             </div>
 
-            <div class="rounded-xl bg-indigo-900 py-4">
-                <div class="flex justify-evenly items-center gap-12">
+            <div class="lg:relative rounded-xl bg-indigo-900 py-4">
+                <div class=" lg:grid lg:justify-start lg:pl-4 flex justify-evenly items-center gap-12">
                     <div class="text-white">
                         <p class="text-xs">{{ title }}</p>
                         <p class="text-2xl">{{ hours }} hrs</p>
                     </div>
                     <div class="text-indigo-300">
-                        <div class="flex justify-end pb-4">
+                        <div class="lg:absolute lg:ml-40 lg:top-5 lg:right-4 flex justify-end pb-4">
                             <img class="w-4" src="@/assets/images/icon-ellipsis.svg" alt="">
                         </div>
                         <p class="text-xs">Last Week - {{ hoursLastWeek }} hrs</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+      colors: {
+        lightRed: '#ff8c66',
+        softBlue: '#56c2e6',
+        lightRedStudy: '#ff5c7c',
+        limeGreen: '#4acf81',
+        violet: '#7536d3',
+        softOrange: '#f1c65b',
+        veryDarkBlue: '#0f1424',
+        darkBlue: '#1c1f4a',
+        desaturatedBlue: '#6f76c8',
+        paleBlue: '#bdc1ff'
+      },
+
       fontFamily: {
         'Rubik': ['Rubik', 'sans-serif']
       },
+      
     },
   },
   plugins: [],
